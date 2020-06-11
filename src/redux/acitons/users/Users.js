77abc5902df8add0e2fb87c2956352efdec3af6b
gzellah <jellyforjully088@yahoo.com>
2020-06-11 19:51:18 +0200

    first commit

--- src/redux/acitons/users/Users.js
+++ src/redux/acitons/users/Users.js
@@ -26,7 +26,7 @@ export const fetchUsersFailure = (errors) =>{
 export const fetchUsers = () =>{
     return (dispatch) =>{
         dispatch(fetchUsersRequest)
-        axios.get('http://jsonplaceholder.typicode.com/users')
+        axios.get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
             const users = response.data;
             console.log(users);
